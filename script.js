af22eeeb5c9ec37c0859c4f733e755463e6c4332
engnogueira <engnogueira.hotmail.com>
2021-12-05 09:54:47 -0300

    Correção requisito 15 e implementação dos requisitos 16,17,18,19

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+/* eslint-disable quotes */
 // CAPTURAS DE ELEMENTOS
 // const getForm = document.getElementsByClassName('trybewarts-login')[0];
 const emailInput = document.getElementById('email-input');
 const passwordInput = document.getElementById('password-input');
-const btnForm = document.getElementById('login-button');
-
-// FUNÇÕES DE VALIDAÇÃO
-function clicarBotao() {
+const btnEnviarCredenciais = document.getElementById('login-button');
+const checkBoxAgreement = document.getElementById('agreement');
+const btnEnviarFormulario = document.getElementById('submit-btn');
+// btnEnviarFormulario.disable = true;
+// FUNÇÕES DE EXECUÇÃO
+function enviarCredenciais() {
   if (
     emailInput.value === 'tryber@teste.com'
     && passwordInput.value === '123456'
@@ -15,7 +18,38 @@ function clicarBotao() {
   alert('Email ou senha inválidos.');
 }
 
+function selecionarCheckbox() {
+  console.log(checkBoxAgreement.checked);
+  if (!checkBoxAgreement.checked) {
+    console.log('botão', btnEnviarFormulario.disable);
+    btnEnviarFormulario.disable = true;
+    return btnEnviarFormulario.disable;
+  }
+  btnEnviarFormulario.removeAttribute('disabled');
+  return btnEnviarFormulario.disable;
+}
+
+function enviarFormulario() {
+  if (!selecionarCheckbox()) {
+    // btnEnviarFormulario.disable = false;
+    alert('Formulário enviado com sucesso!');
+  } else {
+    alert('Você precisa aceitar os termos de uso.');
+  }
+}
 // CAPTURA DE EVENTOS
 window.onload = () => {
-  btnForm.addEventListener('click', clicarBotao);
+  btnEnviarCredenciais.addEventListener('click', enviarCredenciais);
+  checkBoxAgreement.addEventListener('click', selecionarCheckbox);
+  btnEnviarFormulario.addEventListener('click', enviarFormulario);
 };
+
+// eslint-disable-next-line editorconfig/editorconfig
+
+/*
+// CONTAINERS DE FUNÇÕES
+function executarFuncoes(f) {
+  enviarCredenciais();
+  selecionarCheckbox();
+  enviarFormulario();
+} */
